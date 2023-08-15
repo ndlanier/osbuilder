c94708a959acd9efd96f46948d2c4c601391237a
Nathan Lanier <ndlanier@icloud.com>
2023-08-15 15:59:07 -0500

    chore: add requests to controller job

--- controllers/job.go
+++ controllers/job.go
@@ -318,6 +318,14 @@ func (r *OSArtifactReconciler) newBuilderPod(pvcName string, artifact *osbuilder
 		podSpec.Containers = append(podSpec.Containers, artifact.Spec.Resources.Limits.memory)
 	}
 
+	if artifact.Spec.Resources.Requests.cpu {
+		podSpec.Containers = append(podSpec.Containers, artifact.Spec.Resources.Limits.cpu)
+	}
+
+	if artifact.Spec.Resources.Requests.memory {
+		podSpec.Containers = append(podSpec.Containers, artifact.Spec.Resources.Limits.memory)
+	}
+
 	for i, bundle := range artifact.Spec.Bundles {
 		podSpec.InitContainers = append(podSpec.InitContainers, unpackContainer(fmt.Sprint(i), r.ToolImage, bundle))
 	}
