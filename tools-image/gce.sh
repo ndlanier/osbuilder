ead38593a1946b62fbc6139adc312fec8938cda5
Nathan Lanier <ndlanier@icloud.com>
2023-08-25 10:07:56 -0500

    chore: add loop to check for raw image before starting gce image conversion

--- tools-image/gce.sh
+++ tools-image/gce.sh
@@ -3,13 +3,25 @@
 # Transform a raw image disk to gce compatible
 RAWIMAGE="$1"
 OUT="${2:-$RAWIMAGE.gce.raw}"
-cp -rf $RAWIMAGE $OUT
+# NODE_IP="$3"
+# PORT="$4"
+# curl http://10.4.186.183:80/hello-kairos.raw -o $OUT
+# cp -rf $RAWIMAGE $OUT
+
+until [ -f $RAWIMAGE ]
+do
+    sleep 60
+done
+echo "raw image exists, starting conversion."
+
+# GCP requires GCE images base raw image be named disk.raw before packaging in a tarball
+mv $RAW disk.raw
 
 GB=$((1024*1024*1024))
-size=$(qemu-img info -f raw --output json "$OUT" | gawk 'match($0, /"virtual-size": ([0-9]+),/, val) {print val[1]}')
+size=$(qemu-img info -f raw --output json disk.raw | gawk 'match($0, /"virtual-size": ([0-9]+),/, val) {print val[1]}')
 # shellcheck disable=SC2004
 ROUNDED_SIZE=$(echo "$size/$GB+1"|bc)
 echo "Resizing raw image from \"$size\"MB to \"$ROUNDED_SIZE\"GB"
-qemu-img resize -f raw "$OUT" "$ROUNDED_SIZE"G
-echo "Compressing raw image $OUT to $OUT.tar.gz"
-tar -c -z --format=oldgnu -f "$OUT".tar.gz $OUT
+qemu-img resize -f raw disk.raw "$ROUNDED_SIZE"G
+echo "Compressing raw image disk.raw to $OUT.tar.gz"
+tar -c -z --format=oldgnu -f "$OUT".tar.gz disk.raw
